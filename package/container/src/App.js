23979ada0cac330eef134a137e4e2054e4888b02
thejesh <etejesh1@gmail.com>
2021-10-17 22:01:47 +0530

    test the app

--- package/container/src/App.js
+++ package/container/src/App.js
@@ -2,9 +2,9 @@ import React from "react";
 import MarketingApp from "./components/MarketingApp";
 
 export default () => {
-    return <div>
+    return (<div>
             <h1>sai rama</h1>
             <hr />
             <MarketingApp />
-        </div>;
+        </div>);
 };
